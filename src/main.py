6698c08ea330dd8e27837af023a6c9de78cf6c2e
IanO12 <ostrandi@mail.gvsu.edu>
2021-11-07 23:23:42 -0500

    added user database

--- src/main.py
+++ src/main.py
@@ -3,21 +3,33 @@ from time import sleep
 
 import pyrebase
 from kivy.lang import Builder
-from kivy.properties import StringProperty
-from kivy.uix.widget import Widget
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
-from kivy.uix.scrollview import ScrollView
+from firebase_admin import firestore
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+userdb = firestore.client()
 Window.size = (550,800)
-class ScrollableLabel(ScrollView):
-  text = StringProperty('')
-class MyGridLayout(Widget):
-  pass
+firebaseConfig = {
+  'apiKey': "AIzaSyCLaRtvwLMheJOJU2BrRtRAef_ogPn0yrQ",
+  'authDomain': "smartstrength-ae966.firebaseapp.com",
+  'databaseURL': "https://smartstrength-ae966-default-rtdb.firebaseio.com",
+  'projectId': "smartstrength-ae966",
+  'storageBucket': "smartstrength-ae966.appspot.com",
+  'messagingSenderId': "910004572815",
+  'appId': "1:910004572815:web:0d379a3035a6c5f2ddd6fa",
+  'measurementId': "G-N3QTMB57PK"
+}
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+db=firebase.database()
 
 class LoginWindow(Screen):
   def login(self):
-    auth = firebase.auth()
     email=self.ids.user.text
     password=self.ids.password.text
     try:
@@ -31,49 +43,40 @@ class LoginWindow(Screen):
   def clear(self):
     self.ids.user.text = ""
     self.ids.password.text = ""
+
 class SignUpWindow(Screen):
   def create_account(self):
-    auth = firebase.auth()
     email=self.ids.user.text
     password=self.ids.password.text
     print(self.ids.user.text, self.ids.password.text)
     try:
       auth.create_user_with_email_and_password(email,password)
-      print("Success!")
+      user = auth.sign_in_with_email_and_password(email, password)
+      token = user['localId']
+      print(token)
+      userdb.collection('users').document(token).set({'email':email})
+      print("Success!" + auth.current_user['localId'])
       return True
     except:
       print("Email already exists or invalid email")
       self.ids.invalid_email_label.text = "Email already exists or invalid email. Try again"
       return False
-
   def clear(self):
     self.ids.user.text = ""
     self.ids.password.text = ""
+
 class MainWindow(Screen):
-  pass
+  def userId(self):
+    print(auth.current_user['localId'])
 
 class WindowManager(ScreenManager):
   pass
-
-
-firebaseConfig = {
-  'apiKey': "AIzaSyCLaRtvwLMheJOJU2BrRtRAef_ogPn0yrQ",
-  'authDomain': "smartstrength-ae966.firebaseapp.com",
-  'databaseURL': "https://smartstrength-ae966-default-rtdb.firebaseio.com",
-  'projectId': "smartstrength-ae966",
-  'storageBucket': "smartstrength-ae966.appspot.com",
-  'messagingSenderId': "910004572815",
-  'appId': "1:910004572815:web:0d379a3035a6c5f2ddd6fa",
-  'measurementId': "G-N3QTMB57PK"
-}
-firebase = pyrebase.initialize_app(firebaseConfig)
 
 class KivyApp(MDApp):
   def build(self):
     self.theme_cls.theme_style = "Dark"
     self.theme_cls.primary_palette = "BlueGray"
     return Builder.load_file('new_window.kv')
-
 
 if __name__ == '__main__':
     KivyApp().run()
@@ -82,7 +85,7 @@ if __name__ == '__main__':
 
 
 
-db=firebase.database()
+
 
 # storage=firebase.storage()
 
@@ -138,8 +141,8 @@ db=firebase.database()
 # db.child("people").child("person").remove()
 
 # Read
-people=db.child("people").order_by_child("age").start_at("3").get()
-print(people.val())
+# people=db.child("people").order_by_child("age").start_at("3").get()
+# print(people.val())
 
 # people=db.child("people").order_by_child("name").start_at("John").get()
 # for person in people.each():
