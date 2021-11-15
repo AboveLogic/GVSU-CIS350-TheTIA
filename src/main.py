adbc9cd1cf3b5c5321d5a15ecb2e327c7f30843d
IanO12 <ostrandi@mail.gvsu.edu>
2021-11-15 18:50:38 -0500

    create workout function

--- src/main.py
+++ src/main.py
@@ -1,16 +1,12 @@
 import pyrebase
-from kivy.core.image import Image
 from kivy.lang import Builder
 from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
 from firebase_admin import firestore
 import firebase_admin
 from firebase_admin import credentials
-from kivymd.uix.button import MDRectangleFlatButton
 from kivymd.uix.card import MDCard
 
 cred = credentials.Certificate("serviceAccountKey.json")
@@ -30,7 +26,6 @@ firebaseConfig = {
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 db = firebase.database()
-
 
 class LoginWindow(Screen):
     def login(self):
@@ -76,15 +71,20 @@ class MainWindow(Screen):
     layouts = []
     def userId(self):
         print(auth.current_user['localId'])
+    def addExercise(self,instance):
+        print(instance.text)
+        userdb.collection('users').document(auth.current_user['localId']).collection('workouts').document(self.ids.workout_name.text).set({instance.text: firestore.ArrayUnion(['reps', 'sets'])}, merge = True)
+        # userdb.collection('users').document(auth.current_user['localId']).update({'workout': firestore.ArrayUnion([instance.text])})
     def addButton(self):
         if(self.ids.search.text[0] == "#"):
             word = userdb.collection("exercises").where("tags", "array_contains", self.ids.search.text).get()
         else:
             word = userdb.collection("exercises").where("name", "==", self.ids.search.text).get()
         for words in word:
-            layout = Button(text = str(words.get('name')), size_hint_y = None, height = 100)
-            self.ids.widget_list.add_widget(layout)
-            self.layouts.append(layout)
+            txt = str(words.get('name'))
+            button = Button(text = txt, size_hint_y = None, height = 100,on_press=self.addExercise)
+            self.ids.widget_list.add_widget(button)
+            self.layouts.append(button)
     def delButton(self):
         for i in self.layouts:
             self.ids.widget_list.remove_widget(i)
@@ -101,64 +101,3 @@ class KivyApp(MDApp):
 
 if __name__ == '__main__':
     KivyApp().run()
-
-# storage=firebase.storage()
-
-# login
-# email=input("Enter your email ")
-# password=input("Enter your password ")
-# try:
-#   auth.sign_in_with_email_and_password(email, password)
-#   print("Successfully signed in!")
-# except:
-#   print("invalid user or password. Try again")
-#
-# # Signup
-# confirmpass=input("Confirm password")
-# if password==confirmpass:
-#   try:
-#     auth.create_user_with_email_and_password(email,password)
-#     print("Success!")
-#   except:
-#     print("Email already exists")
-
-# Storage
-# upload
-# filename=input("Enter the name of the file you want to upload")
-# cloudfilename=input("Enter the name of the file on the cloud")
-# storage.child(cloudfilename).put(filename)
-#
-# print(storage.child(cloudfilename).get_url(None))
-# download
-# cloudfilename=input("Enter the name of the file you want to download")
-# storage.child(cloudfilename).download("","downloaded.txt")
-
-# reading file
-# cloudfilename=input("Enter the name fo the file you want to read")
-# url=storage.child(cloudfilename).get_url(None)
-# f=urllib.request.urlopen(url).read()
-# print(f)
-
-# Database
-# data={'age': 40, 'address': "New York", 'employed' : True, 'name':"John Smith"}
-# db.child("people").child("ostrandi").set(data)
-# db.child("people").push(data)
-
-# #Update
-# db.child("people").child("ostrandi").update({'name': "Jane"})
-
-# people=db.child("people").get()
-# for person in people.each():
-#   if person.val()['name'] == "JohnSmith":
-#     db.child("people").child(person.key()).update({'name':'Deez'})
-
-# Delete
-# db.child("people").child("person").remove()
-
-# Read
-# people=db.child("people").order_by_child("age").start_at("3").get()
-# print(people.val())
-
-# people=db.child("people").order_by_child("name").start_at("John").get()
-# for person in people.each():
-#   print(person.val()["address"])
