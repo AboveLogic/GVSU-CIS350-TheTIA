48dc19bd916026e7eb363f20a82d90f837e721c5
IanO12 <ostrandi@mail.gvsu.edu>
2021-11-28 18:42:19 -0500

    delete function, favorite page

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 import pyrebase
-from kivy.core.text import Label
 from kivy.lang import Builder
 from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
 from kivy.uix.popup import Popup
 from kivymd.app import MDApp
@@ -46,7 +43,6 @@ class LoginWindow(Screen):
             print("invalid user or password. Try again")
             self.ids.invalid_label.text = "invalid user or password. Try again"
             return False
-
     def clear(self):
         self.ids.user.text = ""
         self.ids.password.text = ""
@@ -64,13 +60,6 @@ class SignUpWindow(Screen):
             token = user['localId']
             print(token)
             userdb.collection('users').document(token).set({'email': email, 'password': password, 'bio':'','picture':''})
-            userdb.collection('users').document(token).collection('workouts').document("Monday").set({"name": "Monday",'yourExercises':{}}, merge = True)
-            userdb.collection('users').document(token).collection('workouts').document("Tuesday").set({"name": "Tuesday",'yourExercises':{}}, merge = True)
-            userdb.collection('users').document(token).collection('workouts').document("Wednesday").set({"name": "Wednesday",'yourExercises':{}}, merge = True)
-            userdb.collection('users').document(token).collection('workouts').document("Thursday").set({"name": "Thursday",'yourExercises':{}}, merge = True)
-            userdb.collection('users').document(token).collection('workouts').document("Friday").set({"name": "Friday",'yourExercises':{}}, merge = True)
-            userdb.collection('users').document(token).collection('workouts').document("Saturday").set({"name": "Saturday",'yourExercises':{}}, merge = True)
-            userdb.collection('users').document(token).collection('workouts').document("Sunday").set({"name": "Sunday",'yourExercises':{}}, merge = True)
             print("Success!" + auth.current_user['localId'])
             return True
         except:
@@ -87,67 +76,101 @@ class P(Popup):
     def addExercise(self):
         if self.ids.reps.text != "" and self.ids.sets.text != "":
             userdb.collection('users').document(auth.current_user['localId']).collection('workouts').document(self.day).set({
+                "name": self.day,
                 "yourExercises": {
-                    self.text: {
+                    self.text.replace(" ","_"): {
                     'reps' :self.ids.reps.text ,
                     'sets': self.ids.sets.text}}}, merge = True)
+class namePopup(Popup):
+    pass
 class MainWindow(Screen):
     def show_popup(self,txt, day):
         pops = P()
         pops.text = txt
         pops.day = day
         pops.open()
-    layouts = []
-    layouts2 = []
-    sets = 0
-    reps = 0
+    def show_name_popup(self):
+        pops = namePopup()
+        pops.open()
+    layout = []
     day = "Monday"
     def userId(self):
         print(auth.current_user['localId'])
     def setDay(self, day):
         self.day = day
     def displayWorkout(self):
-        word = userdb.collection("users").document(auth.current_user['localId']).collection('workouts').where("name" ,"==" ,self.day).get()
-        # print(word)
-        for words in word:
-            # print(words)
-            txt = words.get("yourExercises")
-            for txts in txt:
-                # print(txts)
+        word = userdb.collection("users").document(auth.current_user['localId']).collection('workouts').document(self.day).get()
+        try:
+            txt = word.get("yourExercises")
+            for i in txt:
+                reps = txt[i]["reps"]
+                sets = txt[i]["sets"]
                 card = MDCard(size_hint_y=None, height=100, padding=15)
-                label = MDLabel(text=txts)
+                ex = i.replace("_"," ")+":"+reps+"X"+sets
+                label = MDLabel(text=ex)
                 image = Image(source = "icons\Weight.png")
-                button = Button(text="trash", size_hint_y = None,size_hint_x=None, width = 50, height=50)
+                button = Button(text="trash", size_hint_y = None,size_hint_x=None, width = 50, height=50,on_release=lambda x:self.deleteWorkout(x.parent,x.parent.children[2].text.partition(':')[0]))
                 card.add_widget(label)
                 card.add_widget(image)
                 card.add_widget(button)
                 self.ids.widget_list.add_widget(card)
-                self.layouts.append(card)
-        add_button = Button(text = "add",size_hint_y=None, height=100,on_press=lambda x:self.switchScreen("search","left"))
-        fav_button = Button(text="add to favorites", size_hint_y=None, height=100,on_press=lambda x: self.switchScreen("search", "left"))
+                self.layout.append(card)
+        except:
+            pass
+        print(len(self.ids.widget_list.children))
+        add_button = Button(text="add", size_hint_y=None, height=100,on_release=lambda x: self.switchScreen("search", "left"))
+        fav_button = Button(text="add to favorites", size_hint_y=None, height=100,on_release=lambda x:self.show_name_popup())
         self.ids.widget_list.add_widget(add_button)
-        self.layouts.append(add_button)
-    def addFavorite(self):
+        self.layout.append(add_button)
+        if len(self.ids.widget_list.children) > 1:
+            self.ids.widget_list.add_widget(fav_button)
+            self.layout.append(fav_button)
+    def displayFavorites(self,name):
+        word = userdb.collection("users").document(auth.current_user['localId']).collection('favorites').get()
+        for w in word:
+            button = Button(text=name, size_hint_y=None, height=100)
+            self.ids.favorite_list.add_widget(button)
+            self.layout.append(button)
+        print("WORKED")
+        # for w in word:
+        #     z = w.to_dict()
+        #     # print(word)
+        #     for l in z:
+        #         print(l)
+    def deleteWorkout(self,workout,text):
+        print(text)
+        self.ids.widget_list.remove_widget(workout)
+        w = userdb.collection("users").document(auth.current_user['localId']).collection('workouts').document(self.day).update({"yourExercises.%s"%text.replace(" ","_"):firestore.DELETE_FIELD})
         pass
+    def addFav(self,name):
+        pass
+    def addFavorite(self,name,workout):
+        for i in workout:
+            print(i)
+            userdb.collection('users').document(auth.current_user['localId']).collection('favorites').document(name).set({
+                    i: {
+                        'reps': workout[i]["reps"],
+                        'sets': workout[i]["sets"]}}, merge=True)
     def switchScreen(self,screen,dir):
         self.ids.manager.current = screen
         self.ids.manager.transition.direction=dir
     def addButton(self):
-        searchText = self.ids.search.text.lower()
+        searchText = list(self.ids.search.text.lower().replace(" ","_"))
         if(self.ids.search.text[0] == "#"):
-                word = userdb.collection("exercises").where("tags", "array_contains", searchText).get()
+                word = userdb.collection("exercises").where("tags", "array_contains", "".join(searchText)).get()
         else:
-            word = userdb.collection("exercises").where("name", "==", searchText).get()
+            word = userdb.collection("exercises").where("name", "==", "".join(searchText)).get()
         for words in word:
-            txt = str(words.get('name'))
-            button = Button(text = txt, size_hint_y = None, height = 100,on_press=lambda x:self.show_popup(txt,self.day))
+            txt = str(words.get('name').replace("_", " "))
+            print(txt)
+            button = Button(text = txt, size_hint_y = None, height = 100,on_press=lambda x:self.show_popup(x.text,self.day))
             self.ids.search_list.add_widget(button)
-            self.layouts2.append(button)
+            self.layout.append(button)
     def delButton(self):
-        for i in self.layouts:
+        for i in self.layout:
             self.ids.widget_list.remove_widget(i)
-        for i in self.layouts2:
             self.ids.search_list.remove_widget(i)
+            self.ids.favorite_list.remove_widget(i)
 class WindowManager(ScreenManager):
     pass
 class KivyApp(MDApp):
